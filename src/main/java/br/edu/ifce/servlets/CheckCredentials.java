ba7b43fda4677248d90ef46f5c5fb9df59bf33a3
aluno <aluno@BL3-LABRSF-PC01>
2017-11-30 10:48:31 -0300

    Setado o targeted runtime para o tomcat

---
+++ src/main/java/br/edu/ifce/servlets/CheckCredentials.java
@@ -0,0 +1,40 @@
+package br.edu.ifce.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CheckCredentials
+ */
+public class CheckCredentials extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String dbPassword;
+
+    /**
+     * Default constructor.
+     */
+    public CheckCredentials() {
+        // TODO Auto-generated constructor stub
+    	super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
